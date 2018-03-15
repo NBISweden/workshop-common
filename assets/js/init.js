3baa1cab9007b4a48f9c4dd76ed489e67641ef67
Johan Viklund <johan.viklund@nbis.se>
2018-03-15 17:53:09 +0100

    Removed even more unnecesary JS

--- assets/js/init.js
+++ assets/js/init.js
@@ -13,13 +13,4 @@
 
     $('select').material_select();
   }); // end of document ready
-
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top)
-        }, 1000, 'easeInOutExpo');
-        event.preventDefault();
-    });
 })(jQuery); // end of jQuery name space
