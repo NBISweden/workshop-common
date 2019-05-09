646b172f08c87b1b2b3e744e04fb118772844484
Johan Viklund <johan.viklund@nbis.se>
2019-05-09 10:00:56 +0200

    First stab at dynamic toc

--- assets/js/init.js
+++ assets/js/init.js
@@ -14,3 +14,71 @@
     $('select').material_select();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+(function () {
+    // Find all headings
+    var headings = ['H1', 'H2', 'H3']
+    var levels = [0,0,0];
+    var elems = []
+    for (h of headings) {
+        for (e of document.getElementsByTagName(h)) {
+            elems.push({ element: e});
+        }
+    }
+
+    // Sort headings based on their position in the document
+    elems.sort(function(a,b) {
+        return a.element.getBoundingClientRect().top - b.element.getBoundingClientRect().top
+    });
+
+    // Create table of contents
+    toc = document.getElementById('toc');
+    var id_prefix = 'xyz';
+    var id_num = 0;
+    for (e of elems) {
+        var zero_levels = false;
+        for (i in headings) {
+            if (zero_levels) {
+                levels[i] = 0;
+            }
+            else if (headings[i] == e.element.tagName) {
+                levels[i]++;
+                zero_levels = true;
+            }
+        }
+        if ( e.element.id == "" ) {
+            e.element.id = id_prefix + "-" + levels.join('.');
+        }
+        var anchor = '#' + e.element.id;
+
+        a = document.createElement('a');
+        a.setAttribute('href', anchor);
+        a.innerText = levels.join('.') + ' ' + e.element.innerText;
+
+        li = document.createElement('li');
+        li.appendChild(a);
+        li.classList.add( e.element.tagName );
+
+        toc.appendChild(li);
+        e.li = li;
+    }
+
+    // Scroll listener
+    var selected = elems[0];
+    selected.li.classList.add('selected');
+    window.addEventListener('scroll', function () {
+        var new_selected;
+        for (e of elems) {
+            t = e.element.getBoundingClientRect().top;
+            if (t < window.innerHeight/2) {
+                new_selected = e;
+            }
+        }
+        if (new_selected != selected) {
+            selected.li.classList.remove('selected');
+            new_selected.li.classList.add('selected');
+            selected = new_selected;
+        }
+    });
+})();
+
